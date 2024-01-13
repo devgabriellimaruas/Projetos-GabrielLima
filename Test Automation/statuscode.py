31cecbe9ecb33391b387a3b1d8a39a12647aac10
Gabriel Lima <111135029+gabriellimaruas@users.noreply.github.com>
2024-01-13 17:16:55 -0300

    Adição de Projeto

---
+++ Test Automation/statuscode.py
@@ -0,0 +1,7 @@
+import requests
+
+def status_code():
+    url_ibge = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    response_url = requests.get(url_ibge)
+    status_url = response_url.status_code
+    return status_url
