702fa2f28f4768e6efffb234a81d1bbae00c07fa
gabriellimaruas <limaruasgabriel@gmail.com>
2024-08-21 09:39:52 -0300

    Projetos finalizados

---
+++ BotDiscord/main.py
@@ -0,0 +1,42 @@
+import discord
+from messengerbot import messagebot
+
+
+def iniciar_bot():
+    server_id = 'SERVER ID'
+    channel_id = 'CHANNEL ID'
+    token = 'TOKEN BOT'
+
+    class Client(discord.Client):
+
+        def __init__(self):
+            super().__init__(intents=discord.Intents.default())
+            self.synced = False
+
+        async def on_ready(self):
+            await self.wait_until_ready()
+            if not self.synced:
+                await tree.sync(guild=discord.Object(id=server_id))
+            self.synced = True
+            print(f"Entramos como {self.user}")
+
+            # Envia msg automaticamente
+            target_channel = self.get_channel(int(channel_id))
+            if target_channel:
+                await target_channel.send(f"Mensagem de erro: {await messagebot()}")
+
+            print('Mensagem Enviada')
+
+    aclient = Client()
+    tree = discord.app_commands.CommandTree(aclient)
+
+    @tree.command(guild=discord.Object(id=server_id),
+                  name='teste',
+                  description='Testando')
+    async def slash2(interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Mensagem de erro: {await messagebot()}", ephemeral=False)
+
+    aclient.run(token)
+
+    return 'Mensagem enviada'
---
+++ TestAutomation/README.md
@@ -0,0 +1,22 @@
+# Test Automation
+## Visão Geral
+Esse projeto contém o script de automação para saber se uma API está funcionando corretamente e fazer o envio do seu status por e-mail.
+### Estrutura do Projeto
+* Test Automation: Contém o código-fonte da automação.<br>
+* main.py: Ponto de entrada para a execução do robô.<br>
+* statuscode.py: Contém o código para obter o status da API.
+
+### Requisitos do Sistema
+* Python 3.12.0.
+* Biblioteca smtplib.
+* Biblioteca requests.
+
+### Instalação
+* Instale usando "pip install smtplib".
+* Instale usando "pip install requests".
+
+### Observação
+* Este script de automação só pode ser totalmente automatizado se a aplicação de destino possuir uma API em nuvem acessível. 
+* Certifique-se de que a API está configurada corretamente e disponível para garantir o funcionamento adequado do robô RPA. 
+* Consulte a documentação da API para obter informações sobre autenticação e endpoints necessários.
+* Nesse código a API utilizada é pública.
