2919319f6acd68c421b8ac25409c1e10b16fb85c
devgabriellimaruas <limaruasgabriel@gmail.com>
2025-04-11 21:08:59 -0300

    Subindo novas alterações

--- Coletor-de-perifericos-Kabum/src/services/data_collect_services.py
+++ Coletor-de-perifericos-Kabum/src/services/data_collect_services.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
